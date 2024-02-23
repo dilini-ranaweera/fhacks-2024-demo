a1c5038ffb84b82fa37b25155aa4f36c6e6ea4c6
dilini-ranaweera <dilini.ranaweera.2021@gmail.com>
2024-02-23 00:55:08 -0500

    added an h3

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
           <h1>Hello! This is Dilini's web app.</h1>
           <h2>(and Audrey's too!)</h2>
+          <h3>We love Git!</h3>
       </header>
     </div>
   );
